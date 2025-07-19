afe078a6fd87b001d9c048a220f7b5d0b1b3499f
Sam Leonard <samuelleonard35@gmail.com>
2025-07-19 11:36:28 +0100

    Add support for getting the brightness as well

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,68 @@ use std::{ops::Neg, process::ExitCode};
 const RED: &str = "\x1B[31m";
 const RESET: &str = "\x1B[0m";
 
+const LUMINANCE_FEATURE_CODE: u8 = 0x10;
+
 struct Args {
-    brightness_change: BrightnessChange,
+    action: Action,
     display: Option<usize>,
     list: bool,
+}
+
+#[derive(Clone, Copy)]
+enum Action {
+    Change(BrightnessChange),
+    Get,
+}
+
+impl Action {
+    fn is_noop(self) -> bool {
+        matches!(self, Action::Change(BrightnessChange::Relative(0)))
+    }
+
+    fn execute(self, display: &mut Display, display_no: usize) -> ExitCode {
+        let mut exit_code = ExitCode::SUCCESS;
+
+        let model = display
+            .info
+            .model_name
+            .as_deref()
+            .unwrap_or("Unknown Model");
+
+        let disp = format!("display {display_no} ({model})");
+
+        let Ok(vcp) = display.handle.get_vcp_feature(LUMINANCE_FEATURE_CODE) else {
+            eprintln!("{RED}Timed out waiting for response from {disp}{RESET}");
+            return ExitCode::FAILURE;
+        };
+        let old_value = vcp.value();
+        display.handle.sleep();
+
+        match self {
+            Action::Change(brightness_change) => {
+                let new_value = brightness_change.apply(old_value);
+                if old_value == new_value {
+                    println!("No change needed for {disp}");
+                    return ExitCode::SUCCESS;
+                }
+
+                println!("Changing brighness of {disp} from {old_value} to {new_value}");
+                if let Err(e) = display
+                    .handle
+                    .set_vcp_feature(LUMINANCE_FEATURE_CODE, new_value)
+                {
+                    eprintln!("{RED}Failed to set brightness for {disp}: {e}{RESET}");
+                    exit_code = ExitCode::FAILURE;
+                }
+                display.handle.sleep();
+            }
+            Action::Get => {
+                println!("{disp} is set to {old_value}% brightness");
+            }
+        }
+
+        exit_code
+    }
 }
 
 #[derive(Clone, Copy)]
@@ -18,10 +76,6 @@ enum BrightnessChange {
 }
 
 impl BrightnessChange {
-    fn is_noop(self) -> bool {
-        matches!(self, BrightnessChange::Relative(0))
-    }
-
     fn apply(self, value: u16) -> u16 {
         match self {
             Self::Relative(offset) => {
@@ -39,7 +93,7 @@ fn parse_args() -> Result<Args, lexopt::Error> {
 
     let mut parser = lexopt::Parser::from_env();
     let mut display = None;
-    let mut brightness_change = BrightnessChange::Relative(0);
+    let mut action = Action::Get;
     let mut list = false;
     while let Some(arg) = parser.next()? {
         match arg {
@@ -47,18 +101,18 @@ fn parse_args() -> Result<Args, lexopt::Error> {
                 display = Some(parser.value()?.parse()?);
             }
             Long("inc") => {
-                brightness_change = BrightnessChange::Relative(parser.value()?.parse()?);
+                action = Action::Change(BrightnessChange::Relative(parser.value()?.parse()?));
             }
             Long("dec") => {
-                brightness_change =
-                    BrightnessChange::Relative(parser.value()?.parse::<i16>()?.neg());
+                action = Action::Change(BrightnessChange::Relative(
+                    parser.value()?.parse::<i16>()?.neg(),
+                ));
             }
             Long("set") => {
-                brightness_change = BrightnessChange::Absolute(parser.value()?.parse()?);
+                action = Action::Change(BrightnessChange::Absolute(parser.value()?.parse()?))
             }
-            Short('l') | Long("list") => {
-                list = true;
-            }
+            Long("get") => action = Action::Get,
+            Short('l') | Long("list") => list = true,
             Short('h') | Long("help") => {
                 println!("Usage: ddc-brightness-ctl [-d|--display=NUM] [-l|--list] [--inc=NUM] [--dec=NUM] [--set=NUM]");
                 println!();
@@ -66,6 +120,7 @@ fn parse_args() -> Result<Args, lexopt::Error> {
                 println!("  -d, --display: optionally specify which display to change");
                 println!("                 default operates on all displays");
                 println!("  -l,    --list: list all detected displays and metadata");
+                println!("          --get: get the current brightness");
                 println!("          --set: set brightness to NUM percent");
                 println!("          --inc: increase brightness by NUM percent");
                 println!("          --dec: decrease brightness by NUM percent");
@@ -76,7 +131,7 @@ fn parse_args() -> Result<Args, lexopt::Error> {
     }
 
     Ok(Args {
-        brightness_change,
+        action,
         display,
         list,
     })
@@ -84,7 +139,7 @@ fn parse_args() -> Result<Args, lexopt::Error> {
 
 fn main() -> ExitCode {
     let Args {
-        brightness_change,
+        action,
         display,
         list,
     } = match parse_args() {
@@ -95,7 +150,7 @@ fn main() -> ExitCode {
         }
     };
 
-    if brightness_change.is_noop() && !list {
+    if action.is_noop() && !list {
         return ExitCode::SUCCESS;
     }
 
@@ -137,48 +192,19 @@ fn main() -> ExitCode {
 
     if let Some(n) = display {
         if let Some(disp) = displays.get_mut(n) {
-            change_brightness(disp, n, brightness_change);
+            return action.execute(disp, n);
+        } else {
+            eprintln!("{RED}No display {n}{RESET}");
+            return ExitCode::FAILURE;
         }
-        return ExitCode::SUCCESS;
     }
 
+    let mut exit_code = ExitCode::SUCCESS;
     for (i, mut disp) in displays.into_iter().enumerate() {
-        change_brightness(&mut disp, i, brightness_change);
+        if action.execute(&mut disp, i) == ExitCode::FAILURE {
+            exit_code = ExitCode::FAILURE;
+        }
     }
 
-    ExitCode::SUCCESS
-}
-
-fn change_brightness(display: &mut Display, display_no: usize, change: BrightnessChange) {
-    let model = display
-        .info
-        .model_name
-        .as_deref()
-        .unwrap_or("Unknown Model");
-
-    let disp = format!("display {display_no} ({model})");
-
-    let luminance_feature_code = 0x10;
-    let Ok(vcp) = display.handle.get_vcp_feature(luminance_feature_code) else {
-        eprintln!("{RED}Timed out waiting for response from {disp}{RESET}");
-        return;
-    };
-    display.handle.sleep();
-
-    let old_value = vcp.value();
-    let new_value = change.apply(old_value);
-    if old_value == new_value {
-        println!("No change needed for {disp}");
-        return;
-    }
-
-    println!("Changing brighness of {disp} from {old_value} to {new_value}");
-    if let Err(e) = display
-        .handle
-        .set_vcp_feature(luminance_feature_code, new_value)
-    {
-        eprintln!("{RED}Failed to set brightness for {disp}: {e}{RESET}");
-    }
-
-    display.handle.sleep();
+    exit_code
 }
